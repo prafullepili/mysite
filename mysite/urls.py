c7be486ecf649e4404030c57aa8e7ccf399bbb5c
prafull epili <prafullepili@gmail.com>
2021-10-24 16:06:00 +0530

    login removed

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
+import django
 from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import urls
 
 
 urlpatterns = [
